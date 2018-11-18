6a13ecd60c5323f95256e062f7e9d9079debbd80
Marcus Holmgren <marcus.holmgren1@gmail.com>
2018-11-18 16:39:32 +0100

    Add The Burger Builder Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Layout from "./compontents/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-
-            </div>
+            <Layout>
+                <BurgerBuilder />
+            </Layout>
         );
     }
 }
